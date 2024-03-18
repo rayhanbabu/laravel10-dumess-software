65d51df2839e3d53f0111853779be96215d9c188
rayhanbabu <rayhanbabu458@gmail.com>
2024-03-18 10:29:25 +0600

    update role2

--- app/Http/Middleware/ManagerToken.php
+++ app/Http/Middleware/ManagerToken.php
@@ -27,7 +27,11 @@ class ManagerToken
               $request->headers->set('role',$result->role);
               $request->headers->set('id',$result->id);
               $request->headers->set('manager_username',$result->manager_username);
-              $request->headers->set('role2',$result->role2);
+              if(!empty($result->role2)){
+                $request->headers->set('role2',$result->role2);
+              }else{
+                $request->headers->set('role2','');
+              }
               return $next($request);
         }
     }
