1f5ba8d64bed082aa626ae5d695412da3be86999
rayhanbabu <rayhanbabu458@gmail.com>
2024-04-30 09:23:40 +0600

    update rufend 2

--- resources/views/manager/dashboard.blade.php
+++ resources/views/manager/dashboard.blade.php
@@ -218,7 +218,7 @@
             <div class="card-body">
               <div class="media d-flex">
                 <div class="media-body text-left">
-                  <h3 class="success">{{ $payment1->sum('payble_amount1')+$payment2->sum('payble_amount2')+$extra_payment->sum('amount')+$invoice->sum('withdraw')-($exinvoice->sum('withdraw')) }}TK</h3>
+                  <h3 class="success">{{ $payment1->sum('payble_amount1')+$payment2->sum('payble_amount2')+$extra_payment->sum('amount')+$invoice->sum('withdraw')+$exinvoice->sum('withdraw') }}TK</h3>
                   <span> Total Cash(Manager recived+Total Amount+Resign+Extra Payment)</span>
                 </div>
                   <div class="align-self-center">
